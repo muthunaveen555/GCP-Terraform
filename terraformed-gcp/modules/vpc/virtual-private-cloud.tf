5ba9ca5a46a930daedf9812d71c021b7ead0abdc
Naveen kumar <muthunaveen555@gmail.com>
2020-08-06 09:02:33 +0530

    Update virtual-private-cloud.tf
    Updated tcp ports

--- terraformed-gcp/modules/vpc/virtual-private-cloud.tf
+++ terraformed-gcp/modules/vpc/virtual-private-cloud.tf
@@ -25,7 +25,7 @@ resource "google_compute_firewall" "my-firewall" {
 
   allow {
     protocol = "tcp"
-    ports    = ["22", "80", "8080", "1000-2000"]
+    ports    = ["22", "80"]
   }
 
   source_ranges = ["0.0.0.0/0"]
